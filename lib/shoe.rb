18c740ddb97ece1b5df5d72cedd1fadf8cec2c88
Ayana Bando <ayanaflax@yahoo.com>
2018-05-18 19:06:27 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   BRANDS = []
+  
   def initialize(brand)
     @brand = brand
     BRANDS << brand unless BRANDS.include?(brand)
     binding.pry
   end
-  
+
   def brand
     @brand
       end
